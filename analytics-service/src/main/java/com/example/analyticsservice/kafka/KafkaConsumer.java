71bb4ffe9660f9f221022624a01e2fef779b2c0b
Oktayy16 <oktay16arslan@hotmail.com>
2025-05-14 17:13:54 +0300

    kafka consumer

--- analytics-service/src/main/java/com/example/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/example/analyticsservice/kafka/KafkaConsumer.java
@@ -1,6 +1,8 @@
 package com.example.analyticsservice.kafka;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
@@ -8,13 +10,20 @@ import patient.events.PatientEvent;
 @Service
 public class KafkaConsumer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
     // Belirtilen topicden veri dinleme işlemi bu metot sayesinde olacak. Sanırım Consumer Grouplar bu ıd göre belirlenecek
+    // servis ayağa kalktığında oto burayı dinleyecek
     @KafkaListener(topics = "patient", groupId = "analytics-service")
     public void consumeEvent(byte[] event) {
-        try {
+        try { // eğer gelen event ve PatientEvent eşlezmezse diye bunu kullanıyoruz
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
+
+            // ... perform business related logic
+            log.info("Received Patient Event: [PatientId={}, PatientName={}, PatientEmail={} ]",
+                    patientEvent.getPatientId(),patientEvent.getName(),patientEvent.getEmail());
         } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
+            log.error("Error deserializing event {}",e.getMessage());
         }
     }
 }
