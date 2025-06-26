ae2ce46e8b0ed77db8b6deb93a7f12cd9133cade
Oktayy16 <oktay16arslan@hotmail.com>
2025-06-26 12:44:35 +0300

    jwtKey hatası alıyoruz

--- auth-service/src/main/java/com/example/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/example/auth_service/service/AuthService.java
@@ -1,7 +1,8 @@
 package com.example.auth_service.service;
 
 import com.example.auth_service.dto.LoginRequestDTO;
-import com.example.auth_service.module.User;
+import com.example.auth_service.util.JwtUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,13 +10,21 @@ import java.util.Optional;
 @Service
 public class AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<User> user = userService.findByEmail(loginRequestDTO.getEmail());
-        return null;
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
     }
 }
