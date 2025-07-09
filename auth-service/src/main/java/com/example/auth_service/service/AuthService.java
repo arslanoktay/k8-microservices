ddcccd2b4cb30d73eade78c894e9e4239b75eb6a
Oktayy16 <oktay16arslan@hotmail.com>
2025-07-09 19:51:20 +0300

    token ile 200 dönmüyor kontrol et

--- auth-service/src/main/java/com/example/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/example/auth_service/service/AuthService.java
@@ -2,6 +2,7 @@ package com.example.auth_service.service;
 
 import com.example.auth_service.dto.LoginRequestDTO;
 import com.example.auth_service.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,14 @@ public class AuthService {
                 .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
 
         return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
